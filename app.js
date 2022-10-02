d98870ce555c36355fa714f882d85c1fe3a36f4e
moonsef <bendriouichemonsef@gmail.com>
2022-10-02 19:57:43 +0100

    use air quality router

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const app = express()
+require("dotenv").config();
 
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const { airQuality } = require("./routes");
 
-app.use(express.json())
+app.use(express.json());
+app.use("/api/air-quality", airQuality);
 
 app.listen(process.env.APP_PORT, () => {
-    console.log(`The server is up and running on port ${process.env.APP_PORT}`)
+  console.log(`The server is up and running on port ${process.env.APP_PORT}`);
 });
-
-
-
