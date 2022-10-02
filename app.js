7ab13aeba305d41f238973cdc462465d9ab52757
moonsef <bendriouichemonsef@gmail.com>
2022-10-02 19:00:04 +0100

    add json middlware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 
 require('dotenv').config()
 
+app.use(express.json())
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`The server is up and running on port ${process.env.APP_PORT}`)
